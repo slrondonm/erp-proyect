273ada094e671104c8737309d97137be05a9a95a
Sergio Rondón <sl.rondon.m@gmail.com>
2023-03-31 14:15:12 -0500

    first commit

---
+++ publlic/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Este archivo es el punto de entrada de la aplicación
+ *
+ * PHP version 7.4.23
+ *
+ * @category  Index
+ * @package   ErpProject
+ * @version   1.0.0
+ * @author    Ing. Sergio Rondón
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://www.ideamostuweb.com
+ */
+
+declare(strict_types=1);
